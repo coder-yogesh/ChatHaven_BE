d74d23306e7cc4970f8abff2d72da18b8ef6dce2
Yogesh-S98 <109652643+Yogesh-S98@users.noreply.github.com>
2025-01-07 15:09:10 +0530

    setup server

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 const bodyParse = require('body-parser');
 const dotenv = require('dotenv');
 const authRoutes = require("./auth");
+const serverless = require('serverless-http')
 
 
 const app = express();
@@ -123,7 +124,11 @@ app.get('/proxy-image', async (req, res) => {
       }
 })
 app.use("/api/auth", authRoutes);
+app.get('/', (req, res) => {
+    res.send('Hello from Node.js server on Netlify!');
+});
 const port = 4000;
 app.listen(port, () => {
     console.log('server running');
 })
+module.exports.handler = serverless(app);
