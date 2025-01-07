ad42785eef4e90ab9ac6052b36e1f23d442e0323
Yogesh-S98 <109652643+Yogesh-S98@users.noreply.github.com>
2025-01-07 18:30:23 +0530

    setttt

--- netlify-functions/server.js
+++ netlify-functions/server.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
-const router = express.Router();
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const app = express();
+// const router = express.Router();
 
-// Define routes
-router.get('/', (req, res) => {
-  res.send('Hello from Node.js running on Netlify!');
-});
+// // Define routes
+// router.get('/', (req, res) => {
+//   res.send('Hello from Node.js running on Netlify!');
+// });
 
-router.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
-});
+// router.get('/api/hello', (req, res) => {
+//   res.json({ message: 'Hello from the API!' });
+// });
 
-// Use the router
-app.use('/.netlify/functions/server', router);
+// // Use the router
+// app.use('/.netlify/functions/server', router);
 
-module.exports.handler = serverless(app);
+// module.exports.handler = serverless(app);
