a14afa3d5eef28e3d64ceca4e6695c9001b8989f
Yogesh-S98 <109652643+Yogesh-S98@users.noreply.github.com>
2025-01-09 11:49:20 +0530

    push

--- functions/app.js
+++ functions/app.js
@@ -1,10 +1,60 @@
-const express = require("express");
-const serverless = require("serverless-http");
-const { GoogleGenerativeAI } = require('@google/generative-ai');// Ensure the correct package
-require("dotenv").config();  // For handling environment variables
-
+const express = require('express');
+const serverless = require('serverless-http');
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+const bodyParse = require('body-parser');
 const app = express();
-app.use(express.json());  // To parse JSON request body
+const dotenv = require('dotenv');
+dotenv.config();
+const router = express.Router();
+
+let records = [];
+app.use(bodyParse.json());
+
+router.get('/', (req, res) => {
+  res.send('App is running..');
+});
+
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
+
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
+
+router.get('/proxy-image', async (req, res) => {
+    try {
+        // const { url } = req.query;
+        // if (!url) {
+        //   return res.status(400).send('URL parameter is missing');
+        // }
+        // const response = await axios.get(url, { responseType: 'arraybuffer' });
+        // res.set('Content-Type', response.headers['content-type']);
+        res.send('new api');
+      } catch (error) {
+        res.status(500).send('Error fetching image');
+      }
+})
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Aayush',
+    },
+    {
+      id: '002',
+      name: 'rohit',
+    },
+    {
+      id: '003',
+      name: 'Mohit',
+    },
+  ]);
+});
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
@@ -13,21 +63,19 @@ async function callChatGPT(prompt) {
     const response = await model.generateContent(prompt);
     return response.response.text();
 }
-
-app.post('/chatgpt', async (req, res) => {
+router.post('/chatgpt', async (req, res) => {
     try {
+        console.log('req', req.body);
         const { prompt } = req.body;
 
         if (!prompt) {
-            return res.status(400).json({ error: 'Prompt is required' });
+            return res.status(400).json({ error: 'Promptttttt is required' });
         }
-        
         const response = await callChatGPT(prompt);
         return res.status(200).json({ message: response });
     } catch (error) {
-        console.error(error);  // Log error for debugging
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
-
+app.use('/.netlify/functions/app', router);
 module.exports.handler = serverless(app);
